2d49d2ab9a3aad8c1486c5d7d8ebc29d1227ebf5
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-05-30 23:25:49 -0300

    Cria um component Button

---
+++ src/Components/Button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./styles.module.scss";
+export default function Button() {
+  return (
+    <div>
+      <a href="#" className={styles.neonButton}>
+        Neon
+      </a>
+    </div>
+  );
+}
--- src/Components/Contato/index.js
+++ src/Components/Contato/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Button from "../Button";
+import styles from "./styles.module.scss";
 export default function Contato() {
   return (
-    <div>
+    <div className={styles.containerContato}>
       <h1>Contato</h1>
+      <Button />
     </div>
   );
 }
