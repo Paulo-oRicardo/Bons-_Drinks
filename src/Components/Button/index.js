847fe72a2d76289dd3c6ac1046360ec5285970e6
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-05-31 22:56:51 -0300

    Seta a coloração da página contato

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import styles from "./styles.module.scss";
 export default function Button() {
-  return (
-    <div>
-      <a href="#" className={styles.neonButton}>
-        Neon
-      </a>
-    </div>
-  );
+  return <button className={styles.neonButton}>Enviar</button>;
 }
--- src/Components/Contato/index.js
+++ src/Components/Contato/index.js
@@ -3,9 +3,24 @@ import Button from "../Button";
 import styles from "./styles.module.scss";
 export default function Contato() {
   return (
-    <div className={styles.containerContato}>
+    <section className={styles.containerContato}>
       <h1>Contato</h1>
-      <Button />
-    </div>
+      <div className={styles.divContact}>
+        <img src="./contact-pic.jpg" alt="Maquinha de escrever" />
+
+        <form className={styles.contactForm} action="">
+          <label htmlFor="">Nome:</label>
+          <input type="text" />
+
+          <label htmlFor="">E-mail:</label>
+          <input type="text" />
+
+          <label htmlFor="mensagem">Mensagem:</label>
+          <textarea name="mensagem" id="" cols="20" rows="8" />
+
+          <Button />
+        </form>
+      </div>
+    </section>
   );
 }
